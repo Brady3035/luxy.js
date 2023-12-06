fb8b2cd3159480cde3a3076f1002cb7ba75376d9
Brady3035 <40967324+Brady3035@users.noreply.github.com>
2023-12-06 15:14:45 -0600

    need to run on last 3 folders on desktop pc

--- Python scripts/combine.py
+++ Python scripts/combine.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 # Path to the folder containing the CSV files
-folder_path = '/path/to/your/csv/files/'
+folder_path = 'src/Hourly_data/Hourly Data from 2022-11-01 to 2023-02-28'
 
 # Create an empty DataFrame to store the combined data
 combined_data = pd.DataFrame(columns=['Date', 'kWh', 'Building'])
@@ -15,13 +15,19 @@ for filename in os.listdir(folder_path):
 
         # Read the CSV file into a DataFrame
         file_path = os.path.join(folder_path, filename)
-        df = pd.read_csv(file_path)
+        print(f"Processing file: {file_path}")
 
-        # Add a new 'Building' column with the building name
-        df['Building'] = building_name
+        try:
+            df = pd.read_csv(file_path)
 
-        # Append the data to the combined DataFrame
-        combined_data = combined_data.append(df[['Date', 'kWh', 'Building']], ignore_index=True)
+            # Add a new 'Building' column with the building name
+            df['Building'] = building_name
+
+            # Iterate through each row and append to the combined DataFrame
+            for index, row in df.iterrows():
+                combined_data = combined_data.append(row[['Date', 'kWh', 'Building']], ignore_index=True)
+        except Exception as e:
+            print(f"Error processing file {file_path}: {e}")
 
 # Save the combined data to a new CSV file
 combined_data.to_csv('src/Hourly_data/combined_data.csv', index=False)
