235c6a2b0bbebfa00432d31695aab7adc865e801
Brady3035 <40967324+Brady3035@users.noreply.github.com>
2023-12-14 15:46:12 -0600

    working!!!!

--- Python scripts/checkDuplicate.py
+++ Python scripts/checkDuplicate.py
@@ -1,26 +1,27 @@
 import csv
+from collections import defaultdict
 
 def remove_duplicates(input_file):
     seen_lines = set()
+    output_rows = []
 
     with open(input_file, 'r', newline='') as file:
         reader = csv.reader(file)
-        data = list(reader)
+        header = next(reader)
+        output_rows.append(header)
 
-    with open(input_file, 'w', newline='') as file:
-        writer = csv.writer(file)
-
-        # Write header to the output file
-        writer.writerow(data[0])
-
-        for row in data[1:]:
+        for row_num, row in enumerate(reader, start=2):  # Start from line 2 for proper numbering
             line_string = str(row)
             if line_string not in seen_lines:
                 seen_lines.add(line_string)
-                writer.writerow(row)
+                output_rows.append(row)
+
+    with open(input_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(output_rows)
 
 # Example usage
 input_csv = 'src/Hourly_data/combined_hourly_data_final.csv'
 
 remove_duplicates(input_csv)
-print(f"Duplicate lines removed in-place from {input_csv}.")
+print(f"Duplicate lines removed (keeping only the first occurrence) from {input_csv}.")
