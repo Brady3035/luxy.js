d284495350965c4c57da940f85692d40627194ac
Brady3035 <40967324+Brady3035@users.noreply.github.com>
2023-12-14 22:08:22 -0600

    created dorms and academic building csv

---
+++ Python scripts/filter.py
@@ -0,0 +1,28 @@
+import csv
+
+def filter_and_append(input_file, output_file, buildings_to_keep):
+    with open(input_file, 'r', newline='') as infile, open(output_file, 'w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+
+        # Copy header to the new file
+        header = next(reader)
+        writer.writerow(header)
+
+        # Specify the column index for the 'building' column
+        building_column_index = header.index('building')
+
+        for row in reader:
+            # Check if the building is in the list
+            if row[building_column_index] in buildings_to_keep:
+                writer.writerow(row)
+
+# Example usage
+input_csv = 'src/Hourly_data/combined_hourly_data_final.csv'
+output_csv = 'src/Hourly_data/academic_2022-2023_hourly.csv'
+buildings_to_keep = ["Carnegie Hall Main Electricity Meter", "Humanities Bldg Main Electricity Meter", "Library Main Electricity Meter",
+                     "Music Main Electricity Meter","Old Main Electricity Meter","Olin-Rice Electricity Meter",
+                     "Theater Main Electricity Meter","Weyerhaeuser Hall Main Electricity Meter","Studio Art Building Main Electricity Meter"]
+
+filter_and_append(input_csv, output_csv, buildings_to_keep)
+print(f"Rows with specified buildings appended to {output_csv}.")
